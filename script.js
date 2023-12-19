5638b12bc71b45458158c95cd8f43c09f25ba06b
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-12-19 20:55:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function highlight() {
     //Write your code here
+const boldWords = document.querySelectorAll('strong');
 
+  for (let i = 0; i < boldWords.length; i++) {
+    boldWords[i].classList.add('bold-highlight');
+  }
 
 }
-
-
 function return_normal() {
     //Write your code here
+const boldWords = document.querySelectorAll('strong');
 
-    
+  for (let i = 0; i < boldWords.length; i++) {
+    boldWords[i].classList.remove('bold-highlight');
+  }
+
 }
